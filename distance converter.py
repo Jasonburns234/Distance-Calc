2048c009f5eebe2dae6a3ffa034672004696248a
Jasonburns234 <67003071+Jasonburns234@users.noreply.github.com>
2023-06-29 12:46:40 +0100

    Some quick updates

--- distance converter.py
+++ distance converter.py
@@ -1,5 +1,4 @@
 name = input("Enter your first name: ")
 kilometer = float(input("Enter a distance in kilometers: "))
 miles = kilometer * 0.62137
-
-print("Hi", name.title() + ",", kilometer, "kilometers is", miles, "miles.")
+print(f"Hi {name.title()}! {kilometer} kilometer is equivalent to  {round(miles, 1)} miles.")
